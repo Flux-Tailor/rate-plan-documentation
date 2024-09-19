eb18ae7331108ea7ed45f77271c003548243ec01
Aminata <aminata.cisse@fluxtailor.com>
2024-09-19 16:23:37 -0400

    toc restored & unecessary code removed

--- doc/conf.py
+++ doc/conf.py
@@ -26,6 +26,3 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['OxygenDocs']
-
-# Explicitly specify the master doc
-master_doc = 'index'
